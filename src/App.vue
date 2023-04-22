684c3eb39a2f0969f69c144d2b513ecb51f8500c
Kaustubh Mani Tripathi <72412584+kaustubh16092002@users.noreply.github.com>
2023-04-22 17:55:42 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+      <navbar></navbar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: { Navbar },
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+@import url('@/assets/styles.css');
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="center">
+        <h1>Login page</h1>
+        <v-row> 
+            <v-col cols="10" offset="1" sm="6" offset-sm="3" md="4" offset-md="4">
+                <v-avatar color="primary" size="56" tile>
+                    <v-img src="@/assets/logo.png"></v-img>                
+                    </v-avatar>
+                    <v-text-field label="username"  v-model="username"></v-text-field>
+                    <v-text-field label="password" type="password" v-model="password"></v-text-field>
+                    <template>
+                            <v-btn block class="base-inverted" @click="authenticate()" >LOGIN</v-btn>
+                    </template>
+            </v-col>
+
+
+        </v-row>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: null,
+                password: null,
+            }
+        },
+        methods: {
+            authenticate() {
+                alert("login successfull")
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
